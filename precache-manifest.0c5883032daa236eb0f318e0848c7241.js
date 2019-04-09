a33c8f48487809d53f8379834b7a0b2487135f12
Michael Hazzard <miramardesign@gmail.com>
2019-04-09 10:25:06 -0300

    Updates

---
+++ precache-manifest.0c5883032daa236eb0f318e0848c7241.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "449feae8d7b45aa33cef",
+    "url": "/Front-end/RedditTopFiftyClient/static/js/runtime~main.8acb1a16.js"
+  },
+  {
+    "revision": "f541ff7ac152080b1d4c",
+    "url": "/Front-end/RedditTopFiftyClient/static/js/main.119fec4b.chunk.js"
+  },
+  {
+    "revision": "639cce7ca9097d695c40",
+    "url": "/Front-end/RedditTopFiftyClient/static/js/2.1eec2fd7.chunk.js"
+  },
+  {
+    "revision": "f541ff7ac152080b1d4c",
+    "url": "/Front-end/RedditTopFiftyClient/static/css/main.51155f18.chunk.css"
+  },
+  {
+    "revision": "639cce7ca9097d695c40",
+    "url": "/Front-end/RedditTopFiftyClient/static/css/2.cf8391e7.chunk.css"
+  },
+  {
+    "revision": "40c298c74722a5735974caf8e08e6de5",
+    "url": "/Front-end/RedditTopFiftyClient/index.html"
+  }
+];
