9317c0546b2bfa6a7890f873d765d1a188accae8
Michael Hazzard <miramardesign@gmail.com>
2019-04-10 12:31:58 -0300

    changed drawer type

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+## Deploy to github pages:
+npm run deploy
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/SideNav/SideNav.jsx
+++ src/SideNav/SideNav.jsx
@@ -3,62 +3,48 @@ import RedditList from '../RedditList/RedditList';
 import RedditDesc from '../RedditDesc/RedditDesc';
 
 import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+
+// import AppBar from '@material-ui/core/AppBar';
+// import CssBaseline from '@material-ui/core/CssBaseline';
 import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
-import Hidden from '@material-ui/core/Hidden';
-import IconButton from '@material-ui/core/IconButton';
+// import Drawer from '@material-ui/core/Drawer';
+// import Hidden from '@material-ui/core/Hidden';
+// import IconButton from '@material-ui/core/IconButton';
 import Icon from '@material-ui/core/Icon';
+import Button from '@material-ui/core/Button';
 
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+// import Toolbar from '@material-ui/core/Toolbar';
+// import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 
-const drawerWidth = 240;
-
-const styles = theme => ({
-  root: {
-    display: 'flex',
+const styles = {
+  list: {
+    width: 250,
   },
-  drawer: {
-    [theme.breakpoints.up('sm')]: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
+  fullList: {
+    width: 'auto',
   },
-  appBar: {
-    marginLeft: drawerWidth,
-    [theme.breakpoints.up('sm')]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-    },
-  },
-  menuButton: {
-    marginRight: 20,
-    [theme.breakpoints.up('sm')]: {
-      display: 'none',
-    },
-  },
-  toolbar: theme.mixins.toolbar,
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing.unit * 3,
-  },
-});
+};
 
 
 class SideNav extends React.Component {
 
+  state = {
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  };
 
-  handleDrawerToggle = () => {
-    this.setState(state => ({ mobileOpen: !state.mobileOpen }));
+  toggleDrawer = (side, open) => () => {
+    this.setState({
+      [side]: open,
+    });
   };
 
   constructor(props) {
@@ -86,16 +72,15 @@ class SideNav extends React.Component {
 
   render() {
 
-    const { classes, theme } = this.props;
 
-    const drawer = (
-      <div>
-        <div className={classes.toolbar} />
-        <Divider />
+    const { classes } = this.props;
+
+    const sideList = (
+      <div className={classes.list}>
         <List>
           {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
             <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <Icon>inbox</Icon> :  <Icon>mail</Icon>}</ListItemIcon>
+              <ListItemIcon>{index % 2 === 0 ? <Icon>inbox</Icon> : <Icon>mail</Icon>}</ListItemIcon>
               <ListItemText primary={text} />
             </ListItem>
           ))}
@@ -104,7 +89,29 @@ class SideNav extends React.Component {
         <List>
           {['All mail', 'Trash', 'Spam'].map((text, index) => (
             <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ?<Icon>inbox</Icon> : <Icon>mail</Icon>}</ListItemIcon>
+              <ListItemIcon>{index % 2 === 0 ? <Icon>inbox</Icon> : <Icon>mail</Icon>}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    );
+
+    const fullList = (
+      <div className={classes.fullList}>
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <Icon>inbox</Icon> : <Icon>mail</Icon>}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <Icon>inbox</Icon> : <Icon>mail</Icon>}</ListItemIcon>
               <ListItemText primary={text} />
             </ListItem>
           ))}
@@ -118,88 +125,93 @@ class SideNav extends React.Component {
       return (
         <section>
 
-        <div className={classes.root}>
-        <CssBaseline />
-        <AppBar position="fixed" className={classes.appBar}>
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={this.handleDrawerToggle}
-              className={classes.menuButton}
+          <div>
+            <Button onClick={this.toggleDrawer('left', true)}>Open Left</Button>
+            <Button onClick={this.toggleDrawer('right', true)}>Open Right</Button>
+            <Button onClick={this.toggleDrawer('top', true)}>Open Top</Button>
+            <Button onClick={this.toggleDrawer('bottom', true)}>Open Bottom</Button>
+            <SwipeableDrawer
+              open={this.state.left}
+              onClose={this.toggleDrawer('left', false)}
+              onOpen={this.toggleDrawer('left', true)}
             >
-              <Icon>menu</Icon>
-            </IconButton>
-            <Typography variant="h6" color="inherit" noWrap>
-              Responsive drawer
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <nav className={classes.drawer}>
-          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-          <Hidden smUp implementation="css">
-            <Drawer
-              container={this.props.container}
-              variant="temporary"
-              anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-              open={this.state.mobileOpen}
-              onClose={this.handleDrawerToggle}
-              classes={{
-                paper: classes.drawerPaper,
-              }}
+              <div
+                tabIndex={0}
+                role="button"
+                onClick={this.toggleDrawer('left', false)}
+                onKeyDown={this.toggleDrawer('left', false)}
+              >
+                {sideList}
+              </div>
+            </SwipeableDrawer>
+            <SwipeableDrawer
+              anchor="top"
+              open={this.state.top}
+              onClose={this.toggleDrawer('top', false)}
+              onOpen={this.toggleDrawer('top', true)}
             >
-              {drawer}
-            </Drawer>
-          </Hidden>
-          <Hidden xsDown implementation="css">
-            <Drawer
-              classes={{
-                paper: classes.drawerPaper,
-              }}
-              variant="permanent"
-              open
+              <div
+                tabIndex={0}
+                role="button"
+                onClick={this.toggleDrawer('top', false)}
+                onKeyDown={this.toggleDrawer('top', false)}
+              >
+                {fullList}
+              </div>
+            </SwipeableDrawer>
+            <SwipeableDrawer
+              anchor="bottom"
+              open={this.state.bottom}
+              onClose={this.toggleDrawer('bottom', false)}
+              onOpen={this.toggleDrawer('bottom', true)}
             >
-              {drawer}
-            </Drawer>
-          </Hidden>
-        </nav>
-        <main className={classes.content}>
-          <div className={classes.toolbar} />
-          <Typography paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-         
-          </Typography>
-          <Typography paragraph>
-            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-         
-          </Typography>
-        </main>
-      </div>
-   
+              <div
+                tabIndex={0}
+                role="button"
+                onClick={this.toggleDrawer('bottom', false)}
+                onKeyDown={this.toggleDrawer('bottom', false)}
+              >
+                {fullList}
+              </div>
+            </SwipeableDrawer>
+            <SwipeableDrawer
+              anchor="right"
+              open={this.state.right}
+              onClose={this.toggleDrawer('right', false)}
+              onOpen={this.toggleDrawer('right', true)}
+            >
+              <div
+                tabIndex={0}
+                role="button"
+                onClick={this.toggleDrawer('right', false)}
+                onKeyDown={this.toggleDrawer('right', false)}
+              >
+                {sideList}
+              </div>
+            </SwipeableDrawer>
+          </div>
 
 
+          <div className="SideNavWrapper">
+            <div className="App sidnav-container row">
 
+              <div className="col-md-4">
+                <RedditList onItemClick={this.onItemClick}>
 
-        <div className="SideNavWrapper">
-          <div className="App sidnav-container row">
+                </RedditList>
+              </div>
 
-            <div className="col-md-4">
-              <RedditList onItemClick={this.onItemClick}>
+              <div className="col-md-8">
+                <RedditDesc item={this.state.item}>
 
-              </RedditList>
-            </div>
-
-            <div className="col-md-8">
-              <RedditDesc item={this.state.item}>
-
-              </RedditDesc>
+                </RedditDesc>
+              </div>
             </div>
           </div>
-        </div>
-                  
+
         </section>
       );
-    
+
     }
   }
 }
